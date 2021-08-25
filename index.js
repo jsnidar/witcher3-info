984e43922a1afdd73b1f7837507a8909e4ef5162
Jeremiah <jsnidar@gmail.com>
2021-08-24 20:48:09 -0500

    Define MVP and Stretch Goals

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+
+//MVP
+
 //When DOM Content is loaded fetch characters from the Witcher Api
 
 //Render a list on the left side of the screen of the first 30 characters
-//Render back and next buttons to scroll through pages of characters at the bottom of the list
+//Event Listener 1: Render back and next buttons to scroll through pages of characters at the bottom of the list
 
-//add an event listener to each li, when the li is clicked then the information about that character is rendered to the page in a section
+//Event Listener 2: add an event listener to each li, when the li is clicked then the information about that character is rendered to the page in a section
 
-//add an event listener for them to like that character
-    //liking it saves it to a section at the bottom of the screen titled 'Favorite Characters'
-        //characters are put in a container with a thumbnail and their name.
-        //they display horizontally in boxes when the boxes fill the screen horizontally they make a new row
-            //could I put a slider for this part?
-
-//create a hidden form that has a button filter characters
+//Event Listener 3: create a form that has a button filter characters
     //when you click this button a form appears with:
         //a dropdown list of character traits to search within
             //options: name, gender, race, profession, nationality, appearance
         //text input where they can type in text and search for that attribute name
+
+
+//Stretch
+
+//add an event listener for them to like that character
+    //liking it saves it to a section at the bottom of the screen titled 'Favorite Characters'
+        //characters are put in a container with a thumbnail and their name.
+        //they display in boxes when the boxes fill the screen horizontally they make a new row
+//create a JSON.db to persist the liked characters
+    //pessimistically fetch favorited characters to be displayed
+        //limit to 6 per page
+//limit number of favorited characters to be rendered at once to 6. 
+//create previous and next buttons to scroll though pages of favorited characters.
+
+
